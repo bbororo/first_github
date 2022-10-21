462e9c48c38b898e041d438ca8bc949895b4b58e
bbororo <koogoori54@gmail.com>
2022-10-21 12:38:29 +0900

    upload

---
+++ Downloads/pre_todo/src/main/java/com/bbororo/pre_todo/PreTodoApplication.java
@@ -0,0 +1,13 @@
+package com.bbororo.pre_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreTodoApplication.class, args);
+	}
+
+}
---
+++ Downloads/pre_todo/src/test/java/com/bbororo/pre_todo/PreTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbororo.pre_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
